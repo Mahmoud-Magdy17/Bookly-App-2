c2b42fde03601f3c500a532a13f09ca1d21efa0b
Mahmoud-Magdy17 <MahmoudMagdy176203@gmail.com>
2024-10-09 16:23:48 +0300

    first commit

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+void main(){
+  runApp(const MyBookly());
+}
+
+class MyBookly extends StatelessWidget {
+  const MyBookly({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      
+    );
+  }
+}
